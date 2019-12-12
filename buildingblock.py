a810c37652c17929bd292b41a50da0e337b5c8ca
Lena Tran <asianprincess@Lenas-MBP.domain>
2019-12-11 23:18:42 -0800

    oh my god yes

--- buildingblock.py
+++ buildingblock.py
@@ -1,43 +1,32 @@
 import urllib, urllib2,json
-
-##https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&key=[YOUR_API_KEY]' \
-##--header 'Authorization: Bearer [YOUR_ACCESS_TOKEN]' \
-
-
-# example
-# https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.search.list?
-#         part=snippet
-#         &chart=mostPopular
-#         &regionCode=es
-#         &videoCategoryId=17
-
 def pretty(obj):
     return json.dumps(obj, sort_keys=True, indent=2)
 # my api key :
 APIKEY = "AIzaSyDYZSV5lF3bBGef0C6ndOvXPzZI7jvb1W8"
 baseurl = "https://www.googleapis.com/youtube/v3/videos?"
 
-params= {'key':APIKEY,
-        'method':'youtube.search.list',
-        'part':'snippet',
-        'chart':'mostPopular',
-        'regionCode':'us'
-        }
+def getTopVideos(regionCode, category):
+        params= {'key':APIKEY,
+                'method':'youtube.search.list',
+                'part':'snippet',
+                'chart':'mostPopular',
+                'regionCode': regionCode,
+                'videoCategoryId': category
+                }
 
 
-url = baseurl + urllib.urlencode(params)
-result = urllib2.urlopen(url)
-resultlist = result.read()
-resultdata = json.loads(resultlist)
+        url = baseurl + urllib.urlencode(params)
+        result = urllib2.urlopen(url)
+        resultlist = result.read()
+        resultdata = json.loads(resultlist)
 
-#print pretty(resultdata)
+        youtubeBaseUrl = 'https://www.youtube.com/watch?v='
 
-def getInfo(resultdata):
-        for key in resultdata[['items'][key]['snippet']['title']:
-                s +=(key,resultdata[key])
-        return s
+        print pretty(resultdata)
+
+        for thing in resultdata['items']:
+                print(thing['snippet']['title'])
+                print(youtubeBaseUrl + thing['id'])
 
 
-for x in resultdata:
-        print(getInfo(x))
-         #print resultdata['items'][x]['snippet']['title']
+getTopVideos('us',1)
--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import webapp2
 import os, jinja2
-import json
-
-##https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&key=[YOUR_API_KEY]' \
-##--header 'Authorization: Bearer [YOUR_ACCESS_TOKEN]' \
-
-# my api key : AIzaSyDYZSV5lF3bBGef0C6ndOvXPzZI7jvb1W8
-#https://www.googleapis.com/youtube/v3/videos?
+import logging
+import urllib, urllib2,json
 
 baseurl = 'hhttps://www.googleapis.com/youtube/v3/search'
 
@@ -14,11 +9,54 @@ JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.di
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-# class MainHandler(webapp2.RequestHandler):
-#     def get(self, baseurl = 'https://www.googleapis.com/youtube/v3/search'):
-#
-#         template = JINJA_ENVIRONMENT.get_template('index.html')
-#
+
+APIKEY = "AIzaSyDYZSV5lF3bBGef0C6ndOvXPzZI7jvb1W8"
+baseurl = "https://www.googleapis.com/youtube/v3/videos?"
+
+def getTopVideos(regionCode, category):
+        params= {'key':APIKEY,
+                'method':'youtube.search.list',
+                'part':'snippet',
+                'chart':'mostPopular',
+                'regionCode': regionCode,
+                'videoCategoryId': category
+                }
 
 
-def main():
+        url = baseurl + urllib.urlencode(params)
+        result = urllib2.urlopen(url)
+        resultlist = result.read()
+        resultdata = json.loads(resultlist)
+
+        youtubeBaseUrl = 'https://www.youtube.com/watch?v='
+        embedURL1 ='<iframe id="ytplayer" type="text/html" width="720" height="405" src="https://www.youtube.com/embed/'
+        embedURL2 = '"frameborder="0" allowfullscreen>'
+        data = []
+        for video in resultdata['items']:
+                data.append({
+                    'title': video['snippet']['title'],
+                    'url': youtubeBaseUrl + video['id'],
+                    'videoID': video['id']
+                })
+
+        return data
+
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        payload = {'text': 'hello World'}
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(payload))
+
+    def post(self):
+        country = self.request.get('country')
+        category = self.request.get('category')
+        topVideos = getTopVideos(country, category)
+        data = {'topVideos': topVideos}
+        template = JINJA_ENVIRONMENT.get_template('topViews.html')
+        self.response.write(template.render((data)))
+
+
+
+
+application = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
